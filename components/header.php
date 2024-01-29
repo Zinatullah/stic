029ba313128ec4c747e18d64833e8b82daeec143
Zinatullah <zinatullahakrami@gmail.com>
2024-01-29 10:06:45 +0430

    CSS file renamed

--- components/header.php
+++ components/header.php
@@ -56,7 +56,7 @@ if (isset($_COOKIE['loggedIn'])) {
     <!-- Google Fonts -->
     <!-- <link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet"> -->
 
-    <link rel="stylesheet" href="css/responsiveness.css">
+    <link rel="stylesheet" href="css/responsiveness_mobile.css">
 
     
     <!-- Bootstrap CSS -->
